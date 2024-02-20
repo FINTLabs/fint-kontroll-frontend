9bb2262f0996159ad2cb840c477d602e8a622871
Linda Loftsgarden <linda-fintlabs@users.noreply.github.com>
2024-02-20 15:23:09 +0100

    Added tests for userinfoButton and user info

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -48,4 +48,27 @@ describe('User table Test', () => {
         cy.get('#pagination ul li').should('have.text', 'Forrige12345...10Neste')
         cy.wait(3000)
     });
+
+    it('Select resource and go to page for resource details', () => {
+        cy.visit('http://localhost:3000/users?size=10')
+        cy.get('#userInfoButton-1129').should('exist')
+        cy.wait(1000)
+        cy.get('#userInfoButton-1129').click()
+        cy.wait(1000)
+    });
+    /* describe('User info Test', () => {
+         it('Connect to localhost for user info', (params) => {
+             cy.visit('http://localhost:3000/users/1129')
+         })
+     });
+     it('Check heading on user info page', () => {
+         cy.visit('http://localhost:3000/users/1129')
+         cy.get('h1').should('have.text', 'Brukerinformasjon')
+         cy.wait(2000)
+     });
+     it('Check table heading on user info page', () => {
+         cy.visit('http://localhost:3000/users/1129')
+         cy.get('h2').should('have.text', 'Brukeren er tildelt følgende ressurser:')
+         cy.wait(2000)
+     });*/
 })
