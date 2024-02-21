fcf3ce9a4e5606ba7befa5936618333a8bb7258d
Linda Loftsgarden <linda-fintlabs@users.noreply.github.com>
2024-02-21 14:00:53 +0100

    Added tabs to role info page

--- app/routes/roles.$id.tsx
+++ app/routes/roles.$id.tsx
@@ -1,18 +1,46 @@
 import React from 'react';
-import styles from "~/components/user/user.css"
-import {Heading} from "@navikt/ds-react";
+import {Tabs} from "@navikt/ds-react";
+import {RoleTable} from "~/components/role/RoleTable";
+import {useLoaderData} from "@remix-run/react";
+import {IRolePage} from "~/data/types";
+import {LoaderFunctionArgs} from "@remix-run/router";
+import {fetchRoles} from "~/data/fetch-roles";
+import {json} from "@remix-run/node";
 
-export function links() {
-    return [{rel: 'stylesheet', href: styles}]
+export async function loader({params, request}: LoaderFunctionArgs): Promise<Omit<Response, "json"> & {
+    json(): Promise<any>
+}> {
+    const url = new URL(request.url);
+    const size = url.searchParams.get("size") ?? "10";
+    const page = url.searchParams.get("page") ?? "0";
+    const search = url.searchParams.get("search") ?? "";
+    const response = await fetchRoles(request.headers.get("Authorization"), size, page, search);
+    return json(await response.json());
 }
 
 export default function RolesId() {
+    const rolePage = useLoaderData<IRolePage>();
 
     return (
-        <section className={"content"}>
-            <div className={"toolbar"}>
-                <Heading className={"heading"} level="1" size="xlarge">Gruppeinfo</Heading>
-            </div>
-        </section>
+        <>
+            <Tabs defaultValue="members">
+                <Tabs.List>
+                    <Tabs.Tab
+                        value="members"
+                        label="Medlemmer av gruppen"
+                    />
+                    <Tabs.Tab
+                        value="resources"
+                        label="Tildelte ressurser"
+                    />
+                </Tabs.List>
+                <Tabs.Panel value="members" className="h-24 w-full bg-gray-50 p-4">
+                    <RoleTable rolePage={rolePage}/>
+                </Tabs.Panel>
+                <Tabs.Panel value="resources" className="h-24 w-full bg-gray-50 p-4">
+                    Tildelte ressurser
+                </Tabs.Panel>
+            </Tabs>
+        </>
     );
 }
