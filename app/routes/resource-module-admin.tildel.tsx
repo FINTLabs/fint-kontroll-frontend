5fc558627fbd2a5877306f68a699167bcc1662d4
“Christian-R-Haugland” <christian.haugland@itverket.no>
2024-03-06 10:19:06 +0100

    Removed old solution

--- app/routes/resource-module-admin.tildel.tsx
+++ app/routes/resource-module-admin.tildel.tsx
@@ -1,28 +1,24 @@
-import {Heading, Tabs} from "@navikt/ds-react";
-import {useLoaderData, useOutletContext} from "@remix-run/react";
-import type {IAssignment, IMemberPage, IUserPage} from "~/data/types";
+import {Heading} from "@navikt/ds-react";
+import {useLoaderData} from "@remix-run/react";
+import type {IAssignment, IUserPage} from "~/data/types";
 import type {LoaderFunctionArgs} from "@remix-run/router";
-import {fetchMembers} from "~/data/fetch-roles";
 import {json} from "@remix-run/node";
-import {MemberTable} from "~/components/role/MemberTable";
 import styles from "~/components/user/user.css";
 import {fetchUsers} from "~/data/fetch-users";
-import AssignRightsUserTable from "~/components/resource-module-admin/AssignRightsUserTable";
 import {useState} from "react";
-import {IAssignedUsers} from "~/data/types";
-import {fetchAssignmentUsers} from "~/data/resourceModuleAdmin/resource-module-admin";
 
 export function links() {
     return [{rel: 'stylesheet', href: styles}]
 }
 
 export async function loader({params, request}: LoaderFunctionArgs) {
+    // This loader is not complete. Just a copied version from another file to have a starting point.
     const url = new URL(request.url);
     const search = url.searchParams.get("search") ?? "";
     const size = url.searchParams.get("size") ?? "10";
     const page = url.searchParams.get("page") ?? "0";
-    // const response = await fetchUsers(request.headers.get("Authorization"), size, page, search);
-    const response = await fetchAssignmentUsers(size, page, search);
+    const response = await fetchUsers(request.headers.get("Authorization"), size, page, search);
+    // const response = await fetchAssignmentUsers(size, page, search);
 
 
 
@@ -37,15 +33,11 @@ export default function ResourceModuleAdminTabTildel() {
     console.log(usersPage)
 
     const [newAssignment, setNewAssignment] = useState<IAssignment>({user: null, accessRoleId: "", scopeId: 0, orgUnits:[]})
+
     return (
         <section>
-            <Tabs value={"tildel"}>
-                <Heading className={"heading"} level={"2"} size={"large"}>Tildel rettigheter</Heading>
-                <Tabs.Panel value="tildel" className="h-24 w-full bg-gray-50 p-4">
-                    <AssignRightsUserTable newAssignment={newAssignment} setNewAssignment={setNewAssignment} usersPage={usersPage} />
-                    {/*<MemberTable memberPage={members}/>*/}
-                </Tabs.Panel>
-            </Tabs>
+            <Heading className={"heading"} level={"2"} size={"large"}>Tildel rettigheter</Heading>
+            Innhold kommer
         </section>
     );
 }
