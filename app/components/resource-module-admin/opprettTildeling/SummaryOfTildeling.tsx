e2e05feade20ed58a0794e742155baad4623ab88
“Christian-R-Haugland” <christian.haugland@itverket.no>
2024-04-05 13:17:26 +0200

    Updated UI of Summary

--- app/components/resource-module-admin/opprettTildeling/SummaryOfTildeling.tsx
+++ app/components/resource-module-admin/opprettTildeling/SummaryOfTildeling.tsx
@@ -1,34 +1,48 @@
-import {IResourceModuleAssignment} from "~/data/resourceModuleAdmin/types";
-import {Alert, Heading, List} from "@navikt/ds-react";
+import {IResourceModuleAccessRole, IResourceModuleAssignment} from "~/data/resourceModuleAdmin/types";
+import {Alert, Box, Heading, List} from "@navikt/ds-react";
 
 interface SummaryOfTildelingProps {
     assignment: IResourceModuleAssignment
     missingFields: boolean
+    accessRoles: IResourceModuleAccessRole[]
 }
 
-const SummaryOfTildeling = ({assignment, missingFields}: SummaryOfTildelingProps) => {
+const SummaryOfTildeling = ({assignment, missingFields, accessRoles}: SummaryOfTildelingProps) => {
+    const findAccessRoleById = (id: string): string => {
+        const foundRole: string| undefined = accessRoles.find(role => role.accessRoleId === id)?.name
+        return foundRole ? foundRole : "Noe har gått galt ved rollematch"
+    }
+
     return (
         <div>
-            <Heading size={"medium"}>Oppsummering</Heading>
-            {assignment.user?.firstName && <p>Valgt bruker: {assignment.user?.firstName + " " + assignment.user?.lastName} </p>}
-            {assignment.accessRoleId && <p>Valgt aksessrolle: {assignment.accessRoleId}</p>}
+            <Box
+                padding={"4"}
+                borderWidth={"2"}
+                borderRadius={"large"}
+                className={"tildeling-summary"}
+            >
+                <Heading size={"medium"}>Oppsummering</Heading>
+                {assignment.user?.firstName && <p>Valgt bruker: <b>{assignment.user?.firstName + " " + assignment.user?.lastName}</b> </p>}
+                {assignment.accessRoleId && <p>Valgt aksessrolle: <b>{findAccessRoleById(assignment.accessRoleId)}</b></p>}
 
-            {assignment.orgUnits.length > 0 &&
-                (assignment.includeSubOrgUnits ?
-                    <>
-                        Inkluderte orgenheter - da MED tilhørende underenheter:
-                        <List className={"list-two-columns"}>
-                            {assignment.orgUnits.map(orgunit => <li key={orgunit.id}>{orgunit.name}</li>)}
-                        </List>
-                    </>
-                :
-                    <>Valgte orgenheter:
-                        <List className={"list-two-columns"}>
-                            {assignment.orgUnits.map(orgunit => <li key={orgunit.id}>{orgunit.name}</li>)}
-                        </List>
-                    </>
-                )
-            }
+                {assignment.orgUnits.length > 0 &&
+                    (assignment.includeSubOrgUnits ?
+                        <div className={"summary-org-units-wrapper"}>
+                            <span>Inkluderte orgenheter - da MED tilhørende underenheter:</span>
+                            <List className={"list-two-columns"}>
+                                {assignment.orgUnits.map(orgunit => <li key={orgunit.id}>{orgunit.name}</li>)}
+                            </List>
+                        </div>
+                    :
+                        <div className={"summary-org-units-wrapper"}>
+                            <span>Valgte orgenheter:</span>
+                            <List className={"list-two-columns"}>
+                                {assignment.orgUnits.map(orgunit => <li key={orgunit.id}>{orgunit.name}</li>)}
+                            </List>
+                        </div>
+                    )
+                }
+            </Box>
 
             {missingFields &&
                 <Alert variant={"error"} >
