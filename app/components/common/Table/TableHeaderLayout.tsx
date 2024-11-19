df0584caeb4d45baf4979fbdd2413b4ef0ba598f
IngMyr <ingrid.myrann@gmail.com>
2024-11-19 14:41:32 +0100

    Replace last heandings with layout component

--- app/components/common/Table/TableHeaderLayout.tsx
+++ app/components/common/Table/TableHeaderLayout.tsx
@@ -5,30 +5,46 @@ import OrgUnitFilterModal from "~/components/org-unit-filter/OrgUnitFilterModal"
 
 interface TableHeaderLayoutProps {
     title: string
+    titleAlignment?: "center" | "end" | "start" | undefined
+    subTitle?: string
     SearchComponent?: ReactElement
     FilterComponents?: ReactElement | ReactElement[]
     ChipsFilters?: ReactElement
     orgUnitsForFilter?: IUnitItem[]
     CreateNewButton?: ReactElement
+    LeftAlignedFilters?: ReactElement // TODO: remove this when tabs are implemented
 }
 
 export const TableHeaderLayout = (
     {
         title,
+        titleAlignment = "start",
+        subTitle,
         SearchComponent,
         FilterComponents,
         ChipsFilters,
         orgUnitsForFilter,
-        CreateNewButton
+        CreateNewButton,
+        LeftAlignedFilters
     }: TableHeaderLayoutProps) => {
     return (
         <VStack>
-            <Heading level="1" size="xlarge" spacing>{title}</Heading>
+            <Heading level="1" size="xlarge" align={titleAlignment} spacing={!subTitle}>{title}</Heading>
+            {subTitle && <Heading level="2" size="small">{subTitle}</Heading>}
             <HStack justify={"space-between"} paddingBlock={"4"}>
-                {CreateNewButton ? (
-                    <HStack justify={"end"} align={"end"}>
-                        {CreateNewButton}
-                    </HStack>
+                {(CreateNewButton || LeftAlignedFilters) ? (
+                    <>
+                        {CreateNewButton && (
+                            <HStack justify={"end"} align={"end"}>
+                                {CreateNewButton}
+                            </HStack>
+                        )}
+                        {LeftAlignedFilters && (
+                            <HStack justify={"end"} align={"end"}>
+                                {LeftAlignedFilters}
+                            </HStack>
+                        )}
+                    </>
                 ) : <Spacer/>}
                 <HStack className={"filters"} gap={"4"} justify="end" align="end">
                     {orgUnitsForFilter && <OrgUnitFilterModal orgUnitList={orgUnitsForFilter}/>}
