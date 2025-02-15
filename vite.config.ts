34593da382eb8c03b89041abe88c056132ee02b6
Linda Loftsgarden <linda-fintlabs@users.noreply.github.com>
2025-02-15 11:10:45 +0100

    Updated vite and changed future flags in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { vitePlugin as remix } from '@remix-run/dev';
-import { installGlobals } from '@remix-run/node';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { BASE_PATH } from './environment';
 
-installGlobals();
+//installGlobals();
 
 export default defineConfig({
     base: `${BASE_PATH === '/' ? '' : BASE_PATH}/`,
     plugins: [
         remix({
             basename: `${BASE_PATH.replace(/\/$/, '')}`,
+            future: {
+                v3_fetcherPersist: true,
+                v3_relativeSplatPath: true,
+                v3_throwAbortReason: true,
+            },
         }),
         tsconfigPaths(),
     ],
