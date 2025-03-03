cb7f9f754f915c8e68ff2077c08ab6665dde9268
IngMyr <ingrid.myrann@gmail.com>
2025-03-03 11:45:30 +0100

    Check menu items for landingpage content

--- app/data/fetch-me-info.ts
+++ app/data/fetch-me-info.ts
@@ -1,6 +1,8 @@
-import { BASE_PATH, USER_API_URL } from '../../environment';
+import { BASE_PATH, ROLE_API_URL, USER_API_URL } from '../../environment';
 import { IMeInfo } from '~/data/types/userTypes';
-import { fetchData } from '~/data/helpers';
+import { fetchData, sendRequest } from '~/data/helpers';
+import { USERS } from '~/data/paths';
+import process from 'node:process';
 
 export const fetchMeInfo = async (request: Request): Promise<IMeInfo> => {
     return fetchData(
@@ -9,3 +11,42 @@ export const fetchMeInfo = async (request: Request): Promise<IMeInfo> => {
         'En feil oppstod når vi hentet informasjon om deg, vennligst sjekk at du er logget inn.'
     );
 };
+
+// TODO: use this to check if a user can "se more" in the different tabels
+export const postMyAccessRequest = async (
+    request: Request,
+    requestUrls: {
+        url: string;
+        method: string;
+    }[]
+): Promise<{ url: string; access: boolean }[]> => {
+    const response = await sendRequest({
+        url: `${USER_API_URL}${BASE_PATH}/api/users/me/hasaccess`,
+        method: 'POST',
+        token: request.headers.get('Authorization'),
+        body: {
+            accessRequests: requestUrls.map((url) => ({
+                url: url.url,
+                method: url.method,
+            })),
+        },
+    });
+
+    return response.json();
+};
+
+export const getLandingpageShortcutsAccess = (): { name: string; url: string }[] => [
+    { name: 'users', url: `/api/users` },
+    { name: 'roles', url: `/api/roles` },
+    { name: 'resources', url: `/api/resources/v1` },
+    { name: 'adminResource', url: `/api/resources/admin/v1` },
+
+    // { name: 'userAssignments', url: `${process.env.USER_API_URL}${BASE_PATH}/api/users` },
+    /*    {
+        name: 'userAssignments',
+        url: `http://fint-kontroll-user-catalog:8080${BASE_PATH}/api/users`,
+    },*/
+    // { name: 'resourceAssignments', url: `${basePath}${RESOURCES}` },
+    // { name: 'userAssignmentsStudents', url: `${basePath}${USERS}?userType=STUDENT` },
+    // { name: 'userAssignmentsEmployees', url: `${basePath}${USERS}?userType=EMPLOYEESTAFF` },
+];
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,14 @@
 import { Bleed, Heading, HStack, VStack } from '@navikt/ds-react';
 import { LinkCard, LinkCardGrid } from '~/components/common/LinkCard';
-import { RESOURCES, ROLES, USERS } from '~/data/paths';
+import {
+    RESOURCE_ADMIN,
+    RESOURCE_ADMIN_CREATE_ROLE_ASSIGNMENT,
+    RESOURCES,
+    ROLES,
+    SERVICE_ADMIN,
+    SETTINGS,
+    USERS,
+} from '~/data/paths';
 import {
     PersonGroupIcon,
     PersonIcon,
@@ -13,26 +21,20 @@ import { LoaderFunctionArgs } from '@remix-run/router';
 import { json } from '@remix-run/node';
 import { BASE_PATH } from '../../environment';
 import { fetchMeInfo } from '~/data/fetch-me-info';
-import { useCallback } from 'react';
 
 export async function loader({ params, request }: LoaderFunctionArgs) {
     const me = await fetchMeInfo(request);
 
     return json({
-        me,
+        menuItems: me.menuItems,
         basePath: BASE_PATH === '/' ? '' : BASE_PATH,
     });
 }
 
 export default function Index() {
-    const { basePath, me } = useLoaderData<typeof loader>();
+    const { basePath, menuItems } = useLoaderData<typeof loader>();
 
-    const hasUrl = useCallback(
-        (url: string) => {
-            return me?.menuItems.some((menuItem) => menuItem.url === url);
-        },
-        [me?.menuItems]
-    );
+    console.log('menuItems', menuItems);
 
     return (
         <section className={'full-width'}>
@@ -51,49 +53,115 @@ export default function Index() {
                 </HStack>
             </Bleed>
             <VStack gap="4">
-                <LinkCardGrid color={'var(--beige-60)'} title={'Hva vil du gjøre?'}>
-                    <LinkCard
-                        title={'Administrer brukertildelinger'}
-                        description={
-                            'Se en oversikt over alle brukere. Her kan du gi eller endre tildelinger på brukernivå.'
-                        }
-                        link={`${basePath}${USERS}`}
-                        Icon={<PersonIcon />}
-                        colorProfile={'blue'}
-                    />
-                    <LinkCard
-                        title={'Administrer gruppetildelinger'}
-                        description={
-                            'Se en oversikt over alle grupper, og deres medlemmer og ressurser. Her kan du gi eller endre tilgang til en ressurs på gruppenivå.'
-                        }
-                        link={`${basePath}${ROLES}`}
-                        Icon={<PersonGroupIcon />}
-                        colorProfile={'blue'}
-                    />
-                    <LinkCard
-                        title={'Gi eller endre tilgang til en ressurs'}
-                        description={
-                            'Se en oversikt over alle ressurser i systemet og redigere hvilkne brukere og grupper som har tilgang til dem.'
-                        }
-                        Icon={<TableIcon />}
-                        link={`${basePath}${RESOURCES}`}
-                        colorProfile={'blue'}
-                    />
-                </LinkCardGrid>
-                <LinkCardGrid color={'var(--orange-20)'} title={'Brukertildelinger'}>
-                    <LinkCard
-                        title={'Studenter'}
-                        link={`${basePath}${USERS}?userType=STUDENT`}
-                        Icon={<PersonGroupFillIcon />}
-                        colorProfile={'red'}
-                    />
-                    <LinkCard
-                        title={'Ansatte'}
-                        link={`${basePath}${USERS}?userType=EMPLOYEESTAFF`}
-                        Icon={<PersonSuitFillIcon />}
-                        colorProfile={'red'}
-                    />
-                </LinkCardGrid>
+                {menuItems.some((item) =>
+                    [`${USERS}`, `${ROLES}`, `${RESOURCES}`].includes(item.url)
+                ) && (
+                    <>
+                        <LinkCardGrid color={'var(--beige-60)'} title={'Tilgansstyring'}>
+                            {menuItems.some((item) => item.url === `${USERS}`) && (
+                                <LinkCard
+                                    title={'Administrer brukertildelinger'}
+                                    description={
+                                        'Se en oversikt over alle brukere. Her kan du gi eller endre tildelinger på brukernivå.'
+                                    }
+                                    link={`${basePath}${USERS}`}
+                                    Icon={<PersonIcon />}
+                                    colorProfile={'blue'}
+                                />
+                            )}
+                            {menuItems.some((item) => item.url === `${ROLES}`) && (
+                                <LinkCard
+                                    title={'Administrer gruppetildelinger'}
+                                    description={
+                                        'Se en oversikt over alle grupper, og deres medlemmer og ressurser. Her kan du gi eller endre tilgang til en ressurs på gruppenivå.'
+                                    }
+                                    link={`${basePath}${ROLES}`}
+                                    Icon={<PersonGroupIcon />}
+                                    colorProfile={'blue'}
+                                />
+                            )}
+                            {menuItems.some((item) => item.url === `${RESOURCES}`) && (
+                                <LinkCard
+                                    title={'Gi eller endre tilgang til en ressurs'}
+                                    description={
+                                        'Se en oversikt over alle ressurser i systemet og redigere hvilkne brukere og grupper som har tilgang til dem.'
+                                    }
+                                    Icon={<TableIcon />}
+                                    link={`${basePath}${RESOURCES}`}
+                                    colorProfile={'blue'}
+                                />
+                            )}
+                        </LinkCardGrid>
+                        <LinkCardGrid color={'var(--orange-20)'} title={'Brukertildelinger'}>
+                            <LinkCard
+                                title={'Studenter'}
+                                link={`${basePath}${USERS}?userType=STUDENT`}
+                                Icon={<PersonGroupFillIcon />}
+                                colorProfile={'red'}
+                            />
+                            <LinkCard
+                                title={'Ansatte'}
+                                link={`${basePath}${USERS}?userType=EMPLOYEESTAFF`}
+                                Icon={<PersonSuitFillIcon />}
+                                colorProfile={'red'}
+                            />
+                        </LinkCardGrid>
+                    </>
+                )}
+                {menuItems.some((item) =>
+                    [`${SERVICE_ADMIN}`, `${SETTINGS}`].includes(item.url)
+                ) && (
+                    <LinkCardGrid color={'var(--beige-60)'} title={'Ressursadministrasjon'}>
+                        {menuItems.some((item) => item.url === `${SERVICE_ADMIN}`) && (
+                            <LinkCard
+                                title={'Ressursadministrasjon'}
+                                description={'Se en oversikt over alle ressurser i systemet.'}
+                                Icon={<TableIcon />}
+                                link={`${basePath}${SERVICE_ADMIN}`}
+                                colorProfile={'red'}
+                            />
+                        )}
+                        {menuItems.some((item) => item.url === `${SETTINGS}`) && (
+                            <LinkCard
+                                title={'Innstillinger'}
+                                description={'Se og endre kodeverk, systemtyper og kategorier.'}
+                                Icon={<TableIcon />}
+                                link={`${basePath}${SETTINGS}`}
+                                colorProfile={'red'}
+                            />
+                        )}
+                    </LinkCardGrid>
+                )}
+                {menuItems.some((item) =>
+                    [`${RESOURCE_ADMIN}`, `${RESOURCE_ADMIN_CREATE_ROLE_ASSIGNMENT}`].includes(
+                        item.url
+                    )
+                ) && (
+                    <LinkCardGrid color={'var(--beige-60)'} title={'Roller og rettigheter'}>
+                        {menuItems.some((item) => item.url === `${RESOURCE_ADMIN}`) && (
+                            <LinkCard
+                                title={'Administrer brukere med rolletilknytning'}
+                                description={
+                                    'Se en oversikt over alle brukere med rolletilknytning.'
+                                }
+                                Icon={<TableIcon />}
+                                link={`${basePath}${RESOURCE_ADMIN}`}
+                                colorProfile={'red'}
+                            />
+                        )}
+                        {menuItems.some(
+                            (item) => item.url === `${RESOURCE_ADMIN_CREATE_ROLE_ASSIGNMENT}`
+                        ) && (
+                            <LinkCard
+                                title={'Tildel rettigheter'}
+                                description={'Tildel en rolle til en bruker.'}
+                                Icon={<TableIcon />}
+                                link={`${basePath}${RESOURCE_ADMIN_CREATE_ROLE_ASSIGNMENT}`}
+                                colorProfile={'red'}
+                            />
+                        )}
+                    </LinkCardGrid>
+                )}
             </VStack>
         </section>
     );
